c2fd0945b4e3444ec38a915fefb59d048969bd8d
Keith Brett <keithcbrett@gmail.com>
2026-01-04 23:58:16 +0000

    Added selling, general, administrative prompt - kcbfin #12 - Used in EBITA calculation - Stored in variable selling_general_admin

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,15 @@ main ()
 	int err_synopsis = action::DisplaySynopsis ();
 	if (err_synopsis == -1)
 	{
-		std::cout << "Error in function DisplaySynopsis (returned -1)\n";
+		std::cout << "Error in function Di";
+		std::cout << "splaySynopsis (returned -1)\n";
 		return -1;
 	}
 
 
 	// Prompt user for net sales (used for EBITA calculation)
-	uint64_t net_sales = action::GetNumberStdin ("Enter net sales below:\n");
+	uint64_t net_sales = action::GetNumberStdin
+		("Enter net sales below:\n");
 	if (net_sales == -1)
 	{
 		std::cout << "Error in function GetNumberStdin (net_sales)\n";
@@ -24,10 +26,22 @@ main ()
 
 
 	// Prompt user for cost of merchandise sold (used in EBITA)
-	uint64_t cost_merch_sold = action::GetNumberStdin ("Enter cost of merchandise sold below:\n");
+	uint64_t cost_merch_sold = action::GetNumberStdin
+		("Enter cost of merchandise sold below:\n");
 	if (cost_merch_sold == -1)
 	{
 		std::cout << "Error in function GetNumberStdin (cost_merch_sold)\n";
+		return -1;
+	}
+
+
+	// Prompt user for selling, general, administrative cost (used in EBITA)
+	uint64_t selling_general_admin = action::GetNumberStdin
+		("Enter cost of selling, general, administrative:\n");
+	if (selling_general_admin == -1)
+	{
+		std::cout << "Error in function GetNumb";
+		std::cout << "erStdin (cost_merch_sold)\n";
 		return -1;
 	}
 	return 0;
