13d1fa446d336a0e6b5e2a1038b4f6500c015cc8
Keith Brett <keithcbrett@gmail.com>
2026-01-05 00:39:33 +0000

    Added unit test for function Ebita () - kcbfin #10 - First unit test for module calc - Passes the valid input unit test with the correct result

--- src/unit_tests.cpp
+++ src/unit_tests.cpp
@@ -2,8 +2,16 @@
 
 
 #include "action.h"
+#include "calc.h"
 
 
-TEST (DisplaySynopsisTest, ActionTests) {
-	EXPECT_EQ(0, action::DisplaySynopsis ());
+TEST (DisplaySynopsisValidInp, ActionTests)
+{
+	EXPECT_EQ (0, action::DisplaySynopsis ());
+}
+
+
+TEST (EbitaValidInp, CalcTests)
+{
+	EXPECT_EQ (9790, calc::Ebita (90837, 61054, 18348, 1645));
 }
