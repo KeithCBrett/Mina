3428374cc1124ff19febc41c07404958f71472f3
Keith Brett <keithcbrett@gmail.com>
2026-01-06 00:04:16 +0000

    Added simple error handling for function Ebita () - kcbfin #15 - Added error handling for function Ebita of module calc - If any of the inputs are equal to zero, the function now returns -1

--- src/calc.cpp
+++ src/calc.cpp
@@ -6,6 +6,16 @@ namespace calc {
 			uint64_t inp_selling_general_admin,
 			uint64_t inp_depreciation)
 	{
+		// None of our inputs should be zero. Return error if this is
+		// the case
+		if (inp_net_sales == 0 || inp_cost_merch_sold == 0
+				|| inp_selling_general_admin == 0
+				|| inp_depreciation == 0)
+		{
+			return -1;
+		}
+
+
 		return (inp_net_sales - inp_cost_merch_sold
 				- inp_selling_general_admin - inp_depreciation);
 	}
