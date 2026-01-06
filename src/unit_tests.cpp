130f353999c53cebd18873f48ec4f8c483f1fc28
Keith Brett <keithcbrett@gmail.com>
2026-01-06 00:13:23 +0000

    Added invalid input unit test for function Ebita () - kcbfin #16 - Invalid input unit test for function Ebita of module calc - 4 tests, each of which has a different 0-value input - All pass with correct error symbol output

--- src/unit_tests.cpp
+++ src/unit_tests.cpp
@@ -16,3 +16,12 @@ TEST (EbitaValidInp, CalcTests)
 	EXPECT_EQ (9790, calc::Ebita (90837, 61054, 18348, 1645));
 	EXPECT_EQ (7251, calc::Ebita (77349, 51352, 17053, 1693));
 }
+
+
+TEST (EbitaInvalidInp, CalcTests)
+{
+	EXPECT_EQ (-1, calc::Ebita(0, 61054, 18348, 1645));
+	EXPECT_EQ (-1, calc::Ebita(90837, 0, 18348, 1645));
+	EXPECT_EQ (-1, calc::Ebita(90837, 61054, 0, 1645));
+	EXPECT_EQ (-1, calc::Ebita(90837, 61054, 18348, 0));
+}
